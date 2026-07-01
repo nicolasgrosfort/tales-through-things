966acb222d3e5463ab6f4bbfa5db36cde90a171c
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-01 17:40:13 +0200

    feat: parse response

--- apps/backend/src/utils/controller.ts
+++ apps/backend/src/utils/controller.ts
@@ -1,11 +1,11 @@
 import * as z from "zod";
 
-const Response = z.object({
+const ResponseSchema = z.object({
   ready: z.boolean(),
   question: z.string(),
 });
 
-type ResponseType = z.infer<typeof Response>;
+type ResponseType = z.infer<typeof ResponseSchema>;
 
 export async function sendMessage(input: string): Promise<ResponseType> {
   const res = await fetch("http://localhost:8642/v1/chat/completions", {
@@ -25,13 +25,13 @@ export async function sendMessage(input: string): Promise<ResponseType> {
         { role: "user", content: input },
       ],
       stream: false,
-      format: z.toJSONSchema(Response),
+      format: z.toJSONSchema(ResponseSchema),
     }),
   });
 
   const data = await res.json();
+  const content = data.response.choices[0].message.content;
+  const parsed = ResponseSchema.parse(JSON.parse(content));
 
-  console.log("Raw response data:", data);
-
-  return data;
+  return parsed;
 }
