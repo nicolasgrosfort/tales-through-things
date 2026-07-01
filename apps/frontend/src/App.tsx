86aacc8bd26e367cad2f027a2bcb129651e74f5d
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-01 14:32:53 +0200

    feat: add style

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -6,33 +6,44 @@ function App() {
   const [response, setResponse] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch(`${apiUrl}?question=${encodeURIComponent(question)}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResponse(data.response);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
   return (
-    <>
-      <h1>Tales Through Things</h1>
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Tales Through Things</h1>
       <input
         type="text"
+        className="border border-gray-300 rounded-md p-2 w-full"
         placeholder="Ask a question"
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleFetch();
+          }
+        }}
       />
       <br />
       <br />
       <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => {
-          setIsLoading(true);
-          fetch(`${apiUrl}?question=${encodeURIComponent(question)}`)
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-              setResponse(data.response);
-            })
-            .finally(() => setIsLoading(false));
+          handleFetch();
         }}
       >
         {isLoading ? "Loading..." : "Send"}
       </button>
       <p>{response}</p>
-    </>
+    </main>
   );
 }
 
