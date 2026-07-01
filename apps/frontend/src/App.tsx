4e67fadb234855b28e0b5218c5bf15b99bcd44f1
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-01 13:59:12 +0200

    fix: apiurl

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
+  const apiUrl = `http://${window.location.hostname}:3001`;
+
   return (
     <>
       <h1>Tales Through Things</h1>
       <button
         onClick={() => {
-          fetch("http://localhost:3001/")
+          fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => console.log(data));
         }}
