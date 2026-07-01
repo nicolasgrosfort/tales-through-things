e0ab2164c62980fc59335e88f423ca624f25bc4f
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-01 15:20:11 +0200

    fix: clear all session

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -89,9 +89,7 @@ function App() {
         </button>
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={async () => {
-            await fetch(`${apiUrl}/clear-sessions`);
-          }}
+          onClick={async () => handleClearSessions()}
         >
           Clear All Sessions
         </button>
